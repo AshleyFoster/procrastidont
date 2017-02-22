d6c16a6299ce89c1551508692a3f8c026201d444
Ashley Foster <ashley@ashleyfoster.me>
2017-02-21 22:48:30 -0500

    Add test for task creation

---
+++ spec/features/user_creates_task_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+RSpec.feature "Creating a task" do
+  scenario "user can see task " do
+    user = create(:user)
+    login_as(user, :scope => :user)
+
+     visit '/tasks'
+     click_on 'Create task'
+
+     fill_in :task_title, with: 'Program Rails'
+     fill_in :task_description, with: 'Do some Rails!!'
+     select '10', from: "task_time_4i"
+     select '01', from: "task_time_5i"
+     check 'task_days_of_week_sunday'
+     check 'task_days_of_week_thursday'
+
+     click_button 'Create task'
+
+     expect(page).to have_content 'Program Rails'
+     expect(page).to have_content 'Do some Rails!!'
+     expect(page).to have_content '10:01AM'
+
+     expect(page).to have_css(".day.active", text: "S")
+     expect(page).to have_css(".day.active", text: "TH")
+  end
+end
